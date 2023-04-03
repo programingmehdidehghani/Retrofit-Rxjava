51c6274243f15c5359b8bef9800468d128aedd59
mehdi <dehghani_m@rocketmail.com>
2023-04-03 18:25:43 +0430

    start make package

---
+++ app/src/main/java/com/example/rxjavaretrofit/utils/Constants.java
@@ -0,0 +1,12 @@
+package com.example.rxjavaretrofit.utils;
+
+public class Constants {
+
+    public static final int TIMER_SANIYE = 3000;
+    public static final int TIMER_ARALIK = 1000;
+    public static final String INTERNET_YOK_DIALOG = "internetYokDialog";
+    public static final String CIKIS_DIALOG = "cikisDialog";
+    public static final String TIKLANAN_BILIM_KADINI_TASINANIN_BASLIGI = "tiklananBilimKadini";
+    public static final String BASE_URL = "https://raw.githubusercontent.com/deryacayiroglu/h5190004deryacayiroglu/main/";
+    public static final String SERVIS_API = "BilimKadinlariApi.json";
+}
