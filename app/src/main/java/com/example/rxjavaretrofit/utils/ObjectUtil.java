85416690c418f9672575fc501a5ce44456b0c887
mehdi <dehghani_m@rocketmail.com>
2023-04-03 18:40:25 +0430

    make interface

---
+++ app/src/main/java/com/example/rxjavaretrofit/utils/ObjectUtil.java
@@ -0,0 +1,17 @@
+package com.example.rxjavaretrofit.utils;
+
+import com.google.gson.Gson;
+
+public class ObjectUtil {
+
+    public static String bilimKadiniToJsonString(BilimKadiniModel bilimKadiniModel)
+    {
+        Gson gson = new Gson();
+        return gson.toJson(bilimKadiniModel);
+    }
+    public static BilimKadiniModel jsonStringToBilimKadini(String jsonString)
+    {
+        Gson gson = new Gson();
+        return gson.fromJson(jsonString,BilimKadiniModel.class);
+    }
+}
