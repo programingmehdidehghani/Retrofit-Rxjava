9c251095ac27357a72547862d2660328ae43cf0b
mehdi <dehghani_m@rocketmail.com>
2023-04-03 18:38:51 +0430

    make another utils

---
+++ app/src/main/java/com/example/rxjavaretrofit/utils/NetworkUtil.java
@@ -0,0 +1,18 @@
+package com.example.rxjavaretrofit.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class NetworkUtil {
+
+    public static boolean internetVarMi(Context context){
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if(networkInfo!=null && networkInfo.isAvailable() && networkInfo.isConnected()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
