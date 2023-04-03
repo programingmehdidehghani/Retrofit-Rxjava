e710b00989118c7adac3567a5afe9db4b7b72a70
mehdi <dehghani_m@rocketmail.com>
2023-04-03 18:30:57 +0430

    change names

--- app/src/main/java/com/example/rxjavaretrofit/activities/SplashActivity.java
+++ app/src/main/java/com/example/rxjavaretrofit/activities/SplashActivity.java
@@ -1,8 +1,10 @@
-package com.example.rxjavaretrofit;
+package com.example.rxjavaretrofit.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.rxjavaretrofit.R;
 
 public class SplashActivity extends AppCompatActivity {
 
