e27f1a21f25aa346f33afa7e1d43997c371bb0f1
mehdi <dehghani_m@rocketmail.com>
2023-04-04 13:40:40 +0430

    call init in listactivity

--- app/src/main/java/com/example/rxjavaretrofit/activities/ListActivity.java
+++ app/src/main/java/com/example/rxjavaretrofit/activities/ListActivity.java
@@ -37,6 +37,7 @@ public class ListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        init();
     }
 
     @Override
