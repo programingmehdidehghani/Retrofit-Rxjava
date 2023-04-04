72216fb099bf1f7c8992282ed1baa81b6304c41b
mehdi <dehghani_m@rocketmail.com>
2023-04-04 13:07:48 +0430

    make method rx listactivity

--- app/src/main/java/com/example/rxjavaretrofit/activities/ListActivity.java
+++ app/src/main/java/com/example/rxjavaretrofit/activities/ListActivity.java
@@ -6,11 +6,23 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.rxjavaretrofit.R;
+import com.example.rxjavaretrofit.adapter.BilimKadiniAdaptor;
+import com.example.rxjavaretrofit.model.BilimKadiniModel;
+import com.example.rxjavaretrofit.network.Service;
 import com.example.rxjavaretrofit.utils.AlertDialogUtil;
 import com.example.rxjavaretrofit.utils.Constants;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class ListActivity extends AppCompatActivity {
 
@@ -31,5 +43,53 @@ public class ListActivity extends AppCompatActivity {
                 getString(R.string.alert_dialog_geri_tusu_title),
                 getString(R.string.alert_dialog_geri_tusu_message),
                 Constants.CIKIS_DIALOG);
+    }
+
+    private void init(){
+        imgKapak = findViewById(R.id.imgKapak);
+        txtAciklama = findViewById(R.id.txtAciklama);
+        progressDialogGoster();
+        bilimKadinlariniGetir();
+    }
+
+    public void  bilimKadinlariniGetir()
+    {
+        new Service().getServiceApi().bilimKadinlariniGetir().
+                subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<List<BilimKadiniModel>>() {
+                    List<BilimKadiniModel> bilimKadinlari=new ArrayList<>();
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                    }
+                    @Override
+                    public void onNext(List<BilimKadiniModel> bilimKadiniList) {
+                        bilimKadinlari=bilimKadiniList;
+                    }
+                    @Override
+                    public void onError(Throwable e) {
+                    }
+                    @Override
+                    public void onComplete()
+                    {
+                        if(bilimKadinlari.size()>0) {
+                            initRecycleView(bilimKadinlari);
+                        }
+                        progressDialog.dismiss();
+                    }
+                });
+    }
+
+    private void initRecycleView(List<BilimKadiniModel> bilimKadiniList){
+        recyclerView = findViewById(R.id.rcvBilimKadinlari);
+        BilimKadiniAdaptor bilimKadiniAdaptor = new BilimKadiniAdaptor(bilimKadiniList, getApplicationContext(), new BilimKadiniAdaptor.OnItemClickListener() {
+            @Override
+            public void onClik(int position) {
+                BilimKadiniModel tiklananBilimKadini = bilimKadiniList.get(position);
+                ekranGecisiYap(tiklananBilimKadini);
+            }
+        });
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        recyclerView.setAdapter(bilimKadiniAdaptor);
     }
 }
