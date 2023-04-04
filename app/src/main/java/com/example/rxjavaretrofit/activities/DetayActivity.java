74aceb3d12ff0db5968fd5a83482d35da5179279
mehdi <dehghani_m@rocketmail.com>
2023-04-04 13:36:46 +0430

    finish funtion init

--- app/src/main/java/com/example/rxjavaretrofit/activities/DetayActivity.java
+++ app/src/main/java/com/example/rxjavaretrofit/activities/DetayActivity.java
@@ -1,6 +1,7 @@
 package com.example.rxjavaretrofit.activities;
 
 import android.os.Bundle;
+import android.text.Html;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -9,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.rxjavaretrofit.R;
 import com.example.rxjavaretrofit.model.BilimKadiniModel;
 import com.example.rxjavaretrofit.utils.Constants;
+import com.example.rxjavaretrofit.utils.GlideUtil;
 import com.example.rxjavaretrofit.utils.ObjectUtil;
 
 public class DetayActivity extends AppCompatActivity {
@@ -20,14 +22,23 @@ public class DetayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detay);
+        init();
     }
 
     private void init(){
         String tasinanBilimKadiniString = getIntent().getStringExtra(Constants.TIKLANAN_BILIM_KADINI_TASINANIN_BASLIGI);
         BilimKadiniModel bilimKadiniModel = ObjectUtil.jsonStringToBilimKadini(tasinanBilimKadiniString);
-        
+
         imgKapakDetay = findViewById(R.id.imgKapakDetay);
         txtBaslik = findViewById(R.id.txtBaslik);
         txtDetay = findViewById(R.id.txtDetay);
+
+        txtBaslik.setText(bilimKadiniModel.getAdi());
+        GlideUtil.resimIndiripGoster(getApplicationContext(),bilimKadiniModel.getKapakFotoUrl(),imgKapakDetay);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            txtDetay.setText(Html.fromHtml(bilimKadiniModel.getAciklama(),Html.FROM_HTML_MODE_LEGACY));
+        }
+        else{
+            txtDetay.setText(Html.fromHtml(bilimKadiniModel.getAciklama()));}
     }
 }
