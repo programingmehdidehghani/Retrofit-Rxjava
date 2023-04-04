d53b9f52b9f52e536bb03876cd30cd7f5afac2fd
mehdi <dehghani_m@rocketmail.com>
2023-04-04 12:40:19 +0430

    make class model

--- app/src/main/java/com/example/rxjavaretrofit/network/ServiceApi.java
+++ app/src/main/java/com/example/rxjavaretrofit/network/ServiceApi.java
@@ -1,4 +1,16 @@
 package com.example.rxjavaretrofit.network;
 
+
+import com.example.rxjavaretrofit.model.BilimKadiniModel;
+import com.example.rxjavaretrofit.utils.Constants;
+
+import java.util.List;
+
+import retrofit2.http.GET;
+
 public interface ServiceApi {
+
+    @GET(Constants.SERVIS_API)
+    Observable<List<BilimKadiniModel>> bilimKadinlariniGetir();
+
 }
