9a724403059347395895b519ed0debec7ec7da24
mehdi <dehghani_m@rocketmail.com>
2023-04-04 13:04:49 +0430

    start make view listactivity

--- app/src/main/java/com/example/rxjavaretrofit/activities/ListActivity.java
+++ app/src/main/java/com/example/rxjavaretrofit/activities/ListActivity.java
@@ -1,13 +1,21 @@
 package com.example.rxjavaretrofit.activities;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.rxjavaretrofit.R;
 
 public class ListActivity extends AppCompatActivity {
 
+    ImageView imgKapak;
+    TextView txtAciklama;
+    RecyclerView recyclerView;
+    ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
