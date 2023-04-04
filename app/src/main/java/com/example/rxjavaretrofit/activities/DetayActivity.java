e4116332e9962f037083c1b220f34d3d57245a74
mehdi <dehghani_m@rocketmail.com>
2023-04-04 12:35:10 +0430

    make detay activity

---
+++ app/src/main/java/com/example/rxjavaretrofit/activities/DetayActivity.java
@@ -0,0 +1,16 @@
+package com.example.rxjavaretrofit.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.rxjavaretrofit.R;
+
+public class DetayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detay);
+    }
+}
