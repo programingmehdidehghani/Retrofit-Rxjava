e02d3d2487e616065c795d0d1e82edb28b1ca6cb
mehdi <dehghani_m@rocketmail.com>
2023-04-04 12:51:34 +0430

    continue BilimKadiniViewHolder

---
+++ app/src/main/java/com/example/rxjavaretrofit/adapter/BilimKadiniAdaptor.java
@@ -0,0 +1,51 @@
+package com.example.rxjavaretrofit.adapter;
+
+import android.content.Context;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.rxjavaretrofit.R;
+import com.example.rxjavaretrofit.model.BilimKadiniModel;
+import com.example.rxjavaretrofit.utils.GlideUtil;
+
+import java.util.List;
+
+public class BilimKadiniAdaptor extends RecyclerView.Adapter<BilimKadiniViewHolder> {
+
+    List<BilimKadiniModel> bilimKadinlari;
+    Context context;
+    OnItemClickListener listener;
+
+    public interface OnItemClickListener {
+        void onClik(int position);
+    }
+
+    public BilimKadiniAdaptor(List<BilimKadiniModel> bilimKadinlari, Context context, OnItemClickListener listener) {
+        this.bilimKadinlari = bilimKadinlari;
+        this.context = context;
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public BilimKadiniViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview_bilim_kadini,parent,false);
+        return new BilimKadiniViewHolder(itemView,listener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BilimKadiniViewHolder viewHolder, int position) {
+        viewHolder.txtAdi.setText(bilimKadinlari.get(position).getAdi());
+        viewHolder.txtAlani.setText(bilimKadinlari.get(position).getAlani());
+        viewHolder.txtDogumTarihi.setText(bilimKadinlari.get(position).getDogumTarihi());
+        viewHolder.txtOlumTarihi.setText(bilimKadinlari.get(position).getOlumTarihi());
+        GlideUtil.resimIndiripGoster(context,bilimKadinlari.get(position).getLogoUrl(),viewHolder.imgBilimKadiniLogo);
+    }
+
+    @Override
+    public int getItemCount() {
+        return bilimKadinlari.size();
+    }
+}
